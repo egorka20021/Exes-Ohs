e180829d7a6b2f19087c94d53e750e1b6501036f
egorka20021 <133681517+egorka20021@users.noreply.github.com>
2023-05-16 17:44:10 +0300

    Create script.js

---
+++ script/script.js
@@ -0,0 +1,15 @@
+function XO(str) {
+  var countX = 0;
+  var countO = 0;
+
+  for (var i = 0; i < str.length; i++) {
+    var char = str[i].toLowerCase();
+    if (char === 'x') {
+      countX++;
+    } else if (char === 'o') {
+      countO++;
+    }
+  }
+
+  return countX === countO;
+}
